3c08ecd6ceb4a16e0f24c6fe8365bff03649ece1
rjokhan <jaidonswift@gmail.com>
2025-08-04 01:54:25 +0500

    Фикс скролла к последней карточке

--- users/models.py
+++ users/models.py
@@ -7,6 +7,15 @@ class UserProfile(models.Model):
     subscription_status = models.BooleanField(default=False)
     city = models.CharField(max_length=100)
     subscription_until = models.DateField(null=True, blank=True)
+    telegram_id = models.BigIntegerField(unique=True)
+    first_name = models.CharField(max_length=255, blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+
+    def avatar_url(self):
+        if self.avatar:
+            return self.avatar.url
+        return None
 
     def __str__(self):
         return f"{self.username} ({self.name})"
