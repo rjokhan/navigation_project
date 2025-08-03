9aeecafbc12768c23570da0943057e0c9fa16459
rjokhan <jaidonswift@gmail.com>
2025-08-04 00:56:28 +0500

    Фикс скролла к последней карточке

--- content/validators.py
+++ content/validators.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import ValidationError
 
 def validate_file_size(file):
-    max_size_kb = 2048  # 2 МБ
+    max_size_kb = 5120  # 5 МБ
     if file.size > max_size_kb * 1024:
-        raise ValidationError("Файл слишком большой. Максимум 2 МБ.")
+        raise ValidationError("Слишком большой файл превью! Максимальный размер: 5 МБ.")
